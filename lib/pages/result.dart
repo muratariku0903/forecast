572246a8129e1c5ab10634bb6a4e96f342bf39e8
muratariku0903 <18ba179r@rikkyo.ac.jp>
2022-08-27 00:24:00 +0900

    add weather packaget

--- lib/pages/result.dart
+++ lib/pages/result.dart
@@ -1,7 +1,34 @@
-import 'package:flutter/material.dart';
+import 'dart:html';
 
-class ResultPage extends StatelessWidget {
-  const ResultPage({Key? key}) : super(key: key);
+import 'package:flutter/material.dart';
+import 'package:tmp/classes/geolocation.dart';
+import 'package:weather/weather.dart';
+import 'package:geolocator/geolocator.dart';
+
+class ResultPage extends StatefulWidget {
+  final double lat;
+  final double lon;
+  const ResultPage({Key? key, required this.lat, required this.lon})
+      : super(key: key);
+
+  @override
+  State<ResultPage> createState() => _ResultPageState();
+}
+
+class _ResultPageState extends State<ResultPage> {
+  final WeatherFactory wf = WeatherFactory('1fd9bd35b75f1f31e2fb075342367115',
+      language: Language.JAPANESE);
+  final Geolocation geolocation = Geolocation();
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
+
+  List<> getLocation() {
+
+  }
 
   @override
   Widget build(BuildContext context) {
